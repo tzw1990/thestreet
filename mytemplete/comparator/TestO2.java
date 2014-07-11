2d01c3ede7770dd292e437b77d16148f648a2e1b
z-wang <zihanwangthestreet@gmail.com>
2014-07-11 16:48:21 -0400

    Create TestO2.java

---
+++ mytemplete/comparator/TestO2.java
@@ -0,0 +1,15 @@
+public class TestO2 {
+	private int number;
+	
+	public TestO2(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
